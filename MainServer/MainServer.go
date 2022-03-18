92bfb6a302c1146844d810e97dcf8044fda129aa
Thinkman Wang <wangxf1985@gmail.com>
2022-03-18 15:15:27 +0800

    update

--- MainServer/MainServer.go
+++ MainServer/MainServer.go
@@ -4,40 +4,77 @@ import (
 	"ThinkGOGameServer/serversdk"
 	"ThinkGOGameServer/thinkutils"
 	"ThinkGOGameServer/thinkutils/logger"
+	"github.com/gorilla/websocket"
 	"gopkg.in/ini.v1"
+	"net"
 	"runtime"
-	"sync"
 )
 
 var (
 	log *logger.LocalLogger         = logger.DefaultLogger()
-	g_pSDK *serversdk.GameServerSDK = &serversdk.GameServerSDK{}
+	g_pSDK *serversdk.MainServerSDK = &serversdk.MainServerSDK{}
 )
 
 type Mainerver struct {
-	serversdk.IGameServer
 }
 
 func (this *Mainerver) OnInitGameData() serversdk.GameServerInfo {
 	cfg, err := ini.Load("app.ini")
 	if err != nil {
 		log.Error("Read app.ini failed")
-		
-		return serversdk.GameServerInfo{AppId: 0,
-			Type: "main",
-			Port: 0,
+
+		return serversdk.GameServerInfo{
+			AppId: serversdk.SERVER_MAIN_APPID,
+			Type: serversdk.SERVER_TYPE_MAIN,
+			Port: 8084,
 		}
 	}
-	
-	return serversdk.GameServerInfo{AppId: 0,
-		Type: "main",
+
+	return serversdk.GameServerInfo{
+		AppId: serversdk.SERVER_MAIN_APPID,
+		Type: serversdk.SERVER_TYPE_MAIN,
 		Port: uint32(cfg.Section("main_server").Key("udp_port").MustUint(8084)),
 	}
 }
 
-func (this *Mainerver) SendToGameServer(nAppId uint64, data []byte) {
-	pServer := g_pSDK.RandServer(serversdk.SERVER_TYPE_GAME, 1)
-	logger.Info("%s", thinkutils.JSONUtils.ToJson(pServer))
+func (this *Mainerver) OnUDPMsg(pConn *net.UDPConn, addr net.Addr, data []byte) {
+	log.Info("Receive %d bytes", len(data))
+}
+
+func (this *Mainerver) OnInitWS() serversdk.MainServerInfo {
+	cfg, err := ini.Load("app.ini")
+	if err != nil {
+		log.Error("Read app.ini failed")
+
+		return serversdk.MainServerInfo{
+			Path: "game",
+			Port: 8082,
+			HeartbeatTimeout: 10,
+		}
+	}
+
+	return serversdk.MainServerInfo{
+		Path: "game",
+		Port: uint32(cfg.Section("main_server").Key("ws_port").MustUint(8082)),
+		HeartbeatTimeout: uint32(cfg.Section("main_server").Key("heartbeat").MustUint(10)),
+	}
+}
+
+func (this *Mainerver) OnWSConnect(pConn *websocket.Conn) {
+	log.Info("New Connect")
+}
+
+func (this *Mainerver) OnWSMsg(pConn *websocket.Conn, msg []byte) {
+	log.Info("[%p] recv: %s", pConn, thinkutils.StringUtils.BytesToString(msg))
+}
+
+func (this *Mainerver) OnWSClose(pConn *websocket.Conn) {
+
+	log.Info("Conn closed")
+}
+
+func (this *Mainerver) OnWSTimeout(pConn *websocket.Conn) {
+	log.Info("Heartbeat timeout")
 }
 
 func main() {
@@ -46,9 +83,4 @@ func main() {
 	
 	pMainServer := &Mainerver{}
 	g_pSDK.Init(pMainServer)
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	wg.Wait()
 }
